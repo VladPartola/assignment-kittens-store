4c25364b81bc5afe619e1d925e927ad8d09ca344
Vlad Partola <partolavlad97@gmail.com>
2021-12-05 21:42:45 +0200

    Update start_pipeline

--- start_pipeline.sh
+++ start_pipeline.sh
@@ -1,14 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 echo "------------Starting pipeline--------------"
 
 docker-compose up -d
+sleep 20
+echo "----------Setting up database--------------"
 docker-compose exec app bundle exec rake db:setup db:migrate
 
 echo "----------Testing the application----------"
-docker-compose exec bundle exec rspec >> testlog.txt
+docker-compose exec -T app bundle exec rspec >> testlog.txt
 
 echo "--------Running deploy pipeline------------"
 curl -u vlad:11301def1356894337070fb1a20ea85c58 http://127.0.0.1:8080/job/ruby_app_pipeline/build?token=start-pipeline
 
-ehco "--------Please check the Jenkins-----------"
+echo "--------Please check the Jenkins-----------"
