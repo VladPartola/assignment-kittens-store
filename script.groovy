c79bfae7f386b36e293943023a06f9555e037399
Vlad Partola <partolavlad97@gmail.com>
2021-12-05 17:24:53 +0200

    fix syntax error

--- script.groovy
+++ script.groovy
@@ -15,7 +15,7 @@ def deployApp() {
     def shellCmd = "/bin/bash ./server-script.sh"
     sshagent(['ec2-server-key']) {
         sh "ssh -o StrictHostKeyChecking=no ${server}"
-        sh "scp docker-composeAWS.yaml ${server}:/home/ec2-user"
+        sh "scp docker-composeAWS.yml ${server}:/home/ec2-user"
         sh "scp server-script.sh ${server}:/home/ec2-user"
         sh "ssh -o StrictHostKeyChecking=no ${server} ${shellCmd}"
     }
