4ec5a64216430434211213f6984935ba8dbb4116
Vlad Partola <partolavlad97@gmail.com>
2021-12-05 20:17:05 +0200

    Del folder

--- script.groovy
+++ script.groovy
@@ -15,10 +15,10 @@ def deployApp() {
     def shellCmd = "/bin/bash ./server-script.sh"
     sshagent(['ec2-server-key']) {
         sh "ssh -o StrictHostKeyChecking=no ${server}"
-        sh "scp docker-composeAWS.yml ${server}:/home/ec2-user/application/docker-compose.yml"
-        sh "scp .env ${server}:/home/ec2-user/application/"
-        sh "scp init.sql ${server}:/home/ec2-user/application/"
-        sh "scp -r entrypoints ${server}:/home/ec2-user/application/"
+        sh "scp docker-composeAWS.yml ${server}:/home/ec2-user/docker-compose.yml"
+        sh "scp .env ${server}:/home/ec2-user/"
+        sh "scp init.sql ${server}:/home/ec2-user/"
+        sh "scp -r entrypoints ${server}:/home/ec2-user/"
         sh "scp server-script.sh ${server}:/home/ec2-user/"
         sh "ssh -o StrictHostKeyChecking=no ${server} ${shellCmd}"
     }
